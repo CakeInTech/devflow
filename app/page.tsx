f5e2bccb7ca4f74c5158c30ef572168005160118
CakeInTech <abdulhakimm280@gmail.com>
2025-01-16 13:39:35 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="text-3xl text-violet-700 font-black">
-      Welcome to the world of next.js
+      Welcome to the world of devflow
     </h1>
   );
 }
